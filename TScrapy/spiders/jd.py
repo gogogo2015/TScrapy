cb4caea2dab9b51ef12555ab8773ccda20847cf7
taoxingyu <13612211681@163.com>
2020-07-08 23:13:32 +0800

    爬虫开始

---
+++ TScrapy/spiders/jd.py
@@ -0,0 +1,13 @@
+import scrapy
+
+
+class JdSpider(scrapy.Spider):
+    name = 'jd'
+    allowed_domains = ['jd.com']
+    start_urls = ['http://jd.com/']
+
+    def parse(self, response):
+        pass
+
+
+print('d')
